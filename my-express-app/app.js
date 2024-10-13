50d45d1212dd77c59c421519f43f719d91558f57
Hayat-White <33682739+Hayat-White@users.noreply.github.com>
2024-10-13 03:24:06 -0700

    Update app.js
    Corrected Pathing

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/data', (req, res) => {
+app.get('/', (req, res) => {
     const responseData = {
         message: 'My name is Hayat',
         timestamp: new Date().toISOString()
