abf2b9b65abd9326449a19a3c57533f2d143ad3e
Hayat-White <33682739+Hayat-White@users.noreply.github.com>
2024-10-18 12:11:39 -0700

    Update app.js
    Comments to help explain code!

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -1,17 +1,20 @@
 // Node.js Application
+// Import the express module
 const express = require('express');
+// Create an instance of an Express application
 const app = express();
-
+// Define a GET route for the root URL ('/')
 app.get('/', (req, res) => {
+    // Create a JSON response object with a message and the current Unix timestamp
     const responseData = {
         message: 'My name is Hayat',
         timestamp: Math.floor(Date.now())
     };
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(responseData)); // Minified JSON
+    res.setHeader('Content-Type', 'application/json'); // Set the Content-Type header to indicate that the response will be JSON
+    res.send(JSON.stringify(responseData)); // Send the response with the JSON object, minified
 });
 
 const PORT = 80;
-app.listen(PORT, () => {
+app.listen(PORT, () => { // Start the server and listen on the specified port, printing a message when the server starts
     console.log(`Server is running on http://localhost:${PORT}`);
 });
